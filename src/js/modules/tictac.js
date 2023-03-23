79770258fdd167982e56d201e830072a88fe458b
Aleksanr <Aloofkot@yandex.ru>
2023-03-23 20:39:01 +0300

    tictac win option

--- src/js/modules/tictac.js
+++ src/js/modules/tictac.js
@@ -1,6 +1,7 @@
 export const tictac = () => {
     const board = document.querySelector("#board");
     const party = document.querySelectorAll("[data-party]")
+
     let arrMoves = Array.from({ length: 9 }, (_, i) => i + 1)
     let allFreeMoves = Array.from({ length: 9 }, (_, i) => i + 1)
     let startGame = false
@@ -17,6 +18,7 @@ export const tictac = () => {
     const boxes = document.querySelectorAll('[data-num]')
 
     const aiSkynet = (num) => {
+
         allFreeMoves = arrMoves.filter(e => Number.isInteger(e))
 
         if (allFreeMoves.length > 0) {
@@ -38,8 +40,26 @@ export const tictac = () => {
                 [3, 5, 7],
             ]
 
+            const restartGame = () => {
+                if (window.confirm("Хотите начать игру заново?")) {
+                    window.location.reload();
+                }
+            };
+
             const findArr = (haystack, arr) => {
                 return arr.filter(v => haystack.includes(v));
+            };
+
+            const winArr = (paty) => MM.map(item => {
+                const arrItem = item.filter(e => arrMoves[e - 1] == paty)
+                return arrItem
+            }).filter(e => e.length == 3).flat()
+
+
+            const aiMove = (num) => {
+                const node = document.querySelector(`[data-num='${num}']`)
+                arrMoves[num - 1] = aiParty
+                node.classList.add(aiParty);
             };
 
             const currentMoves = MM.filter(e => e.find(item => item == parsed))
@@ -54,64 +74,42 @@ export const tictac = () => {
                 return arrItem
             })
 
-            // const crit = arrFoCrit.filter(e => e.length == 1).flat()
-
             const arrCrit = arrFoCrit.filter(e => e.length == 1).flat()
             const crit = findArr(allFreeMoves, arrCrit)
-            // const five = arrFreeBox.filter(e => e.find(el => el == 5))
             const odd = arrFreeBox.filter(e => e.find(el => el % 2 !== 0)).flat()
 
-            // console.log(odd)
-
-            // let arr1 = [1, 2, 3];
-            // let arr2 = [2, 3];
-
-
-            // console.log(five)
-
-            // console.log(findOne(allFreeMoves, crit))
-
-
-            // console.log(arrFreeBox)
-
-            // console.log(allFreeMoves)
             if (crit.length > 0) {
-                const num = crit[0]
-                const node = document.querySelector(`[data-num='${num}']`)
-                arrMoves[num - 1] = aiParty
-                node.classList.add(aiParty);
+                aiMove(crit[0])
                 console.log('сработал крит')
             } else if (arrMoves[4] == 5) {
-                const node = document.querySelector(`[data-num='5']`)
-                arrMoves[4] = aiParty
-                node.classList.add(aiParty);
+                aiMove(5)
                 console.log('сработал 5')
             } else if (odd.length > 0) {
-                const node = document.querySelector(`[data-num='${odd[0]}']`)
-                arrMoves[odd[0] - 1] = aiParty
-                node.classList.add(aiParty);
+                aiMove(odd[0])
                 console.log('сработал нечет')
             } else if (allFreeMoves.length > 0) {
-                const node = document.querySelector(`[data-num='${allFreeMoves[0]}']`)
-                arrMoves[allFreeMoves[0] - 1] = aiParty
-                node.classList.add(aiParty);
+                aiMove(allFreeMoves[0])
                 console.log('свободные')
             }
 
-
-            console.log(allFreeMoves)
-
-
-
+            if (winArr(currentParty).length == 3) {
+                setTimeout(function () {
+                    alert("Победил кожанный мешок!!")
+                    restartGame()
+                }, 1);
+            } else if (winArr(aiParty).length == 3) {
+                setTimeout(function () {
+                    alert("Победил скайнет!!")
+                    restartGame()
+                }, 1);
+            }
 
         } else if (allFreeMoves.length == 0) {
-            alert("ФСЁ!!")
+            setTimeout(function () {
+                alert("НИЧЬЯ!!")
+                restartGame()
+            }, 1);
         }
-
-        // console.log(allFreeMoves.length)
-
-
-
     }
 
     party.forEach(item => {
